6f45789f9513bca6a56270d7a1da6082b1910c5b
Grégory <claudejurado@orange.fr>
2017-05-10 17:28:47 +0200

    envoie d'un reponse du serveur via node

--- server.js
+++ server.js
@@ -1,61 +1,69 @@
 var express = require("express");
+var http = require("http").Server(app);
 var app = express();
+
+var jade = require("jade");
 
 var nodefs = require("fs");
 var bodyparser = require("body-parser");
 var db = "inscription.json";
 
-app.get("/",function (req, res){
-
-	res.sendFile(__dirname+"/")});
-
-app.use(express.static(__dirname+"/"))
 app.listen(3000, function(){
 	console.log("yipikai server ok");
 })
 
-app.post("/", function(req, res){
+app.use(express.static(__dirname+"/"))
+app.use(bodyparser.urlencoded({extend:false}));
+app.use(bodyparser.json());
+app.set("view", "./view");
+app.set("view engine", "jade");
 
+app.post("/clients", function(req, res){
+
+		res.sendFile(__dirname+"/clients.jade");
+
+});
+
+app.get("/",function (req, res){
+
+	res.render("clients", {name : "zoubida", lastname : "zoubiii"})
+
+});
+
+// function inscription(req,res){
+// 	var data = req.body;
+// 	console.log(data)
+// 	console.log(req)
+// 	var inscrits= 
+// 		{
+
+// 			name : data.name,
+// 			forname : data.forname,
+// 			login : data.login,
+// 			password : data.password,
+// 			day : data.dayBirthdate,
+// 			month : data.monthBirthdate,
+// 			year : data.yearBirthdate
+// 			//sexe: data.sexe
+
+// 		}
+// 		console.log(inscrits)
+// 	nodefs.readFile(function(err, data){
+
+// 		obj= JSON.parse(data);
+// 		if (err)
+// 		{
+// 			res.send("error");
+// 		}
+// 		obj.push(addCustomer);
+// 		json= JSON.stringify(obj);
+// 		nodefs.writefile(json, function(err){
+// 			if (err){
+// 				res.send ("error");
+// 			}else{
+// 				res.send("success");
+// 			};
+// 		});	
+// 	});
 	
-		inscription(db, req, res);
-
-		res.sendFiles(__dirname+"/");
-	
-
-})
-function inscription(req,res){
-	var data = req.body;
-	console.log(data);
-	var inscrits= 
-		{
-
-			name : data.name,
-			forname : data.forname,
-			login : data.login,
-			password : data.password,
-			day : data.dayBirthdate,
-			month : data.monthBirthdate,
-			year : data.yearBirthdate
-			//sexe: data.sexe
-
-		}
-
-	nodefs.readFile(dir, function(err, data){
-
-		obj= JSON.parse(data);
-		if (err)
-		{
-			res.send("error");
-		}
-		obj.push(addCustomer);
-		json= JSON.stringify(obj);
-		nodefs.writefile(dir, json, function(err){
-			if (err){
-				res.send ("error");
-			}else{
-				res.send("success");
-			};
-		});	
-	});
-	
-}
+// }
